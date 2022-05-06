ad8bc5f7e930ecf22f44abc6c2181a647ff9e64b
cbibbs94 <89940267+cbibbs94@users.noreply.github.com>
2022-05-06 15:18:46 -0700

    Completed assignment, print still does not work when printing request

--- ballpython/reptile.py
+++ ballpython/reptile.py
@@ -1,7 +1,57 @@
-from flask import Blueprint
+from flask import (Blueprint , request, redirect)
+from ballpython import models
+import sys
 
 bp = Blueprint('reptile', __name__, url_prefix='/reptiles')
 
-@bp.route('/')
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    return 'The landing page'
+    #POST Handling
+    
+    if request.method == 'POST':
+        print(request)
+        new_reptile = models.Reptile(
+            common_name = request.form['common_name'],
+            scientific_name = request.form['scientific_name'],
+            conservation_status = request.form['conservation_status'],
+            native_habitat = request.form['native_habitat'],
+            fun_fact = request.form['fun_fact']
+        )
+
+        #add to database
+        models.db.session.add(new_reptile)
+        models.db.session.commit()
+
+        return redirect('/reptiles')
+    
+    #GET Handling
+    found_reptiles = models.Reptile.query.all()
+
+    reptile_dict = {'reptiles': []}
+
+    for reptile in found_reptiles:
+        reptile_dict["reptiles"].append({
+            'common_name': reptile.common_name,
+            'scientific_name' : reptile.scientific_name,
+            'conservation_status' : reptile.conservation_status,
+            'native_habitat' : reptile.native_habitat,
+            'fun_fact': reptile.native_habitat
+        })
+    
+    return reptile_dict
+
+
+
+@bp.route('/<int:id>')
+def show(id):
+    reptile = models.Reptile .query.filter_by(id=id).first()
+
+    reptile_dict = {
+        'common_name': reptile.common_name,
+        'scientific-name' : reptile.scientific_name,
+        'conservation_status' : reptile.conservation_status,
+        'native_habitat' : reptile.native_habitat,
+        'fun_fact' : reptile.fun_fact
+    }
+
+    return reptile_dict
